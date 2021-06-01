48b5c0209584af7e1184edc50d1b9e597678f2fd
Kaz DeBear <kazdb@me.com>
2021-05-31 18:06:59 -0700

    Created API routes, fixed users controller

--- app/controllers/api/users_controller.rb
+++ app/controllers/api/users_controller.rb
@@ -1,2 +1,26 @@
 class Api::UsersController < ApplicationController
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      login!(@user)
+      render :show
+    else
+      render json: @user.errors.full_messages, status: 401
+    end
+  end
+
+  def show
+    @user = selected_user
+  end
+
+  private
+  
+  def selected_user
+    User.find(params[:id])
+  end
+  
+  def user_params
+    params.require(:user).permit(:username, :email, :password)
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  namespace :api, default: {format: :json} do
+    resources :users, only: [:create, :show]
+    resources :session, only: [:create, :destroy]
+  end
+
   root to:
     'static_pages#root'
 end
