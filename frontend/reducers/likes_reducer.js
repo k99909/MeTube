3918b428a92d4af5d1e440e0a5d53414a3427996
Kaz DeBear <kazdb@me.com>
2021-08-05 16:45:53 -0700

    clean up

--- frontend/reducers/likes_reducer.js
+++ frontend/reducers/likes_reducer.js
@@ -6,7 +6,7 @@ import {
 
 const likesReducer = (state = {}, action) => {
     Object.freeze(state)
-    let newState = Object.assign({}, state)
+    let newState = Object.assign({}, state);
 
     switch (action.type) {
         case RECEIVE_LIKES:
