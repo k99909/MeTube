bc108d52244f0a24c7c2c379fbc0885e6cacdbb7
Kaz DeBear <kazdb@me.com>
2021-08-05 16:49:17 -0700

    clean up

--- frontend/actions/session_actions.js
+++ frontend/actions/session_actions.js
@@ -5,7 +5,7 @@ import {
 } from '../utils/session';
 
 
-export const RECEIVE_CURRENT_USER = "RECEIVE_CURRENT_USER"
+export const RECEIVE_CURRENT_USER = "RECEIVE_CURRENT_USER";
 export const LOGOUT_CURRENT_USER = "LOGOUT_CURRENT_USER"
 export const RECEIVE_SESSION_ERRORS = "RECEIVE_SESSION_ERRORS";
 export const CLEAR_ERRORS = "CLEAR_ERRORS";
