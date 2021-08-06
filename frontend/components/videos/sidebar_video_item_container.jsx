3583e8f660d985e6dc814950ee601b1869029a8b
Kaz DeBear <kazdb@me.com>
2021-08-06 11:54:30 -0700

    clean up

--- frontend/components/videos/sidebar_video_item_container.jsx
+++ frontend/components/videos/sidebar_video_item_container.jsx
@@ -5,12 +5,12 @@ import { fetchUser } from '../../actions/user_actions'
 const mSTP = (state, ownProps) => {
     return {
         uploader: state.entities.users[ownProps.video.uploader_id]
-    }
-}
+    };
+};
 
 const mDTP = (dispatch, ownProps) => ({
     fetchVideos: () => dispatch(fetchVideos()),
     fetchUser: userId => dispatch(fetchUser(userId))
-})
+});
 
 export default connect(mSTP, mDTP)(SidebarVideoItem);
