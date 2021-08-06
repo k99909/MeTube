4f0728522ae0a76d940db2713f6f478d0ca47d1a
Kaz DeBear <kazdb@me.com>
2021-08-06 11:51:41 -0700

    clean up

--- frontend/components/header/greeting_container.jsx
+++ frontend/components/header/greeting_container.jsx
@@ -4,7 +4,7 @@ import Greeting from './greeting';
 import { logout } from '../../actions/session_actions';
 
 const mapStateToProps = state => ({
-    currentUser: state.entities.users[state.session.id]
+    currentUser: state.entities.users[state.session.id];
 });
 
 const mapDispatchToProps = dispatch => ({
