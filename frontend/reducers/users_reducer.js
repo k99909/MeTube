36abbc45214925ec43a200ceb5641248eba038de
Kaz DeBear <kazdb@me.com>
2021-08-11 21:49:37 -0700

    code cleanup

--- frontend/reducers/users_reducer.js
+++ frontend/reducers/users_reducer.js
@@ -2,7 +2,7 @@ import { RECEIVE_CURRENT_USER } from '../actions/session_actions';
 import { RECEIVE_USER } from '../actions/user_actions';
 
 export default (state = {}, action) => {
-    Object.freeze(state)
+    Object.freeze(state);
     switch (action.type) {
         case RECEIVE_CURRENT_USER:
             return Object.assign({}, state, { [action.user.id]: action.user });
