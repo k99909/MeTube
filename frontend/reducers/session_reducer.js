45da8eb061d12e9a16e5dc8cb7bb4350100f09d8
Kaz DeBear <kazdb@me.com>
2021-08-11 21:48:41 -0700

    code cleanup

--- frontend/reducers/session_reducer.js
+++ frontend/reducers/session_reducer.js
@@ -12,7 +12,7 @@ export default (state = _nullUser, action) => {
     Object.freeze(state);
     switch (action.type) {
         case RECEIVE_CURRENT_USER:
-            return Object.assign({}, {id: action.user.id})
+            return Object.assign({}, {id: action.user.id});
         case LOGOUT_CURRENT_USER:
             return _nullUser;
         default:
