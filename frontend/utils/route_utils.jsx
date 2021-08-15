2d87c1bfe3d152d1b203747834cdd477382f20bc
Kaz DeBear <kazdb@me.com>
2021-08-14 19:35:22 -0700

    code explanation

--- frontend/utils/route_utils.jsx
+++ frontend/utils/route_utils.jsx
@@ -2,9 +2,13 @@ import React from 'react';
 import { connect } from 'react-redux';
 import { Redirect, Route, withRouter } from 'react-router-dom';
 
+// mSTP mapping session id to props
+
 const mapStateToProps = state => ({
     loggedIn: Boolean(state.session.id)
 })
+
+// Creating 
 
 const Auth = ({ loggedIn, path, component: Component }) => (
     <Route
